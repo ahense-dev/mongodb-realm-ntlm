c8315c8df238bd0b9fa551ada676d9e7f5931c7c
Alexander Hense <ahense@gmail.com>
2022-02-16 09:33:42 +0100

    Change www-authenticate header to Www-Authenticate
    MongoDB Realm capitalizes header keys, so that www-authenticate actually becomes Www-Authenticate.

--- httpntlm.js
+++ httpntlm.js
@@ -66,11 +66,11 @@ exports.method = function(method, options, finalCallback){
 		}
 
 
-		if(!res.headers['www-authenticate'])
+		if(!res.headers['Www-Authenticate'])
 			return callback(new Error('www-authenticate not found on response of second request'));
 
 		// parse type2 message from server:
-		var type2msg = ntlm.parseType2Message(res.headers['www-authenticate'], callback); //callback only happens on errors
+		var type2msg = ntlm.parseType2Message(res.headers['Www-Authenticate'], callback); //callback only happens on errors
 		if(!type2msg) return; // if callback returned an error, the parse-function returns with null
 
 		// create type3 message:
